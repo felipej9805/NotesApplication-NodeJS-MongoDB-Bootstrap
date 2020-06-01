872bf38da34e72d20ac013bbb354aaa68bf66249
Felipe <felipejm9805@gmail.com>
2020-06-01 09:00:25 -0700

    Update database.js file to connect using a env file pass on a parameter

--- src/database.js
+++ src/database.js
@@ -2,8 +2,7 @@
  * Modulo me permite conectar a mongodb
  */
 const mongoose = require('mongoose');
-const MONGO_URL='mongodb://172.17.0.2/notes-db-app';
-
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/notes-db-app';
 /**
  * Coenctarme a una direccion de internet, mongodb conectate a localhost y al nombre de la base de datos, si no existe
  * la crea
@@ -13,7 +12,7 @@ const MONGO_URL='mongodb://172.17.0.2/notes-db-app';
  * UseFindAndModify
  * Es tan solo para el funcionamiento de la biblioteca
  */
-mongoose.connect(MONGO_URL, {
+mongoose.connect(mongoUrl, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useFindAndModify: false,
