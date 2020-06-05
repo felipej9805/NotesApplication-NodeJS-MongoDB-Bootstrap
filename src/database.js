57fa2cc09a6a7d30f7e38ac63f3a15d1fd35ef6a
Felipe <felipejm9805@gmail.com>
2020-06-05 13:39:13 -0700

    Modify to connect with mlab(Atlas)

--- src/database.js
+++ src/database.js
@@ -2,7 +2,9 @@
  * Modulo me permite conectar a mongodb
  */
 const mongoose = require('mongoose');
-const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/notes-db-app';
+const mongoUrl = process.env.MONGO_URL || 'mongodb+srv://root:password@cluster0-fvw5p.mongodb.net/notes-db-app?retryWrites=true&w=majority';
+
+//const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/notes-db-app';
 /**
  * Coenctarme a una direccion de internet, mongodb conectate a localhost y al nombre de la base de datos, si no existe
  * la crea
